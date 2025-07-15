f438a8d4e3a15f3ecc1d07a2defee30eeb8e6a21
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-07-15 09:31:55 -0300

    Ajuste da nomenclatura das pastas. Finalizado o módulo de instruções.

---
+++ W3 Schools - Experimente Você Mesmo/Exemplo 03 - Instruções JavaScript/exemplo03.js
@@ -0,0 +1,11 @@
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+
+document.getElementById("mensagem").innerHTML = 
+"Essa é uma mensagem com mais de 80 caracteres.";
+
+function minhaFuncao() {
+    document.getElementById("mensagem").innerHTML = "Olá, Mundo!";
+}
