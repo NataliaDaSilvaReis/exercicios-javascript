3f9a97b8a5ce23084b2edbe98a4e85f93e36a5d5
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-07-15 08:48:50 -0300

    Finalizado primeiro desafio do Curso em Vídeo - JavaScript.

---
+++ Exercícios/Desafio das mensagens/main.js
@@ -0,0 +1,11 @@
+function Botao1() {
+    alert("Você clicou no 1º botão!");
+}
+
+function Botao2() {
+    alert("Você clicou no 2º botão!");
+}
+
+function Botao3() {
+    alert("Você clicou no 3º botão!");
+}
