b99606f731232981e616b59fba3ea4d73677cbd4
Natalia da Silva Reis <nataliadasilvareis@gmail.com>
2025-07-15 08:14:20 -0300

    Finalizado o módulo output.

---
+++ Experimente Você Mesmo/JavaScript Output/exemplo02.js
@@ -0,0 +1,13 @@
+document.getElementById("demonstracao").innerHTML = "Olá, Mundo!";
+
+document.getElementById("demonstracao2").innerText = "Olá, Mundo!";
+
+function Sobrescrever() {
+    document.write("Sobrescrito!");
+}
+
+function Alertar() {
+    alert("Alerta!");
+}
+
+console.log(5 + 6);
